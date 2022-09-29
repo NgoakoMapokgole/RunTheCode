63d884f347eadb0b0eaa033ef2c9b33af2559df1
AmogelangMphela <100419327+AmogelangMphela@users.noreply.github.com>
2022-09-29 22:43:45 +0200

    Update StopWatch.java

--- StopWatch.java
+++ StopWatch.java
@@ -6,35 +6,28 @@ public class StopWatch{
     double secondspassed;
     public StopWatch(){
     }
+    
 
     public Instant start(){
         start = Instant.now(); 
         return start;
-    }
+    }//starts the stopwatch and returns the time it started at
 
     public Instant end(){
         end = Instant.now(); 
         return end;
-    }
+    }//stops the stopwatch and returns the time that it was stopped
     
     public double timeElapsed(){
         Duration timeElapsed = Duration.between(start, end);
         secondspassed = timeElapsed.toSeconds();
         return secondspassed;
-    }
+    } //calculates the time between the time the watch was started and when it was stopped
 
     public void rating(){
-        while(secondspassed < 20 ){
-            Duration timeElapsed = Duration.between(start, end);
-            secondspassed = timeElapsed.toSeconds();
-            end();
-            if(secondspassed == 10.0){
-                System.out.println("You have spent" + " " + secondspassed + " seconds " + " banking");
-            }
-        }
-
+        System.out.println("You have spent" + " " + secondspassed + " seconds " + " banking");
         System.out.println("Your session has ended");
-    }
+    }//this can be used for rating once the program is completed
 
     public String toString(){
        return String.format("%s",secondspassed);
